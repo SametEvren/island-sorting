c27954213bf9a72bf2be9039159d17e2ce18ecf7
Samet <sametevren9@gmail.com>
2023-03-01 06:29:45 +0300

    feat: Level 5 created with question marks.

--- Assets/Scripts/Utility/ColorDictionary.cs
+++ Assets/Scripts/Utility/ColorDictionary.cs
@@ -10,12 +10,12 @@ namespace Utility
             {SortingColor.Red ,Color.red},
             {SortingColor.Green, Color.green},
             {SortingColor.Blue, Color.blue},
-            {SortingColor.Yellow, new Color(0.992f,0.725f,0.153f)},
+            {SortingColor.Yellow, Color.yellow},
             {SortingColor.Purple, new Color(0.333f,0.145f,0.51f,1)},
             {SortingColor.Pink, new Color(1,0.753f,0.796f,1)},
             {SortingColor.Black, Color.black},
             {SortingColor.White, Color.white},
-            {SortingColor.Orange, new Color(1,0.647f,0,1)},
+            {SortingColor.Orange, new Color(0.992f,0.725f,0.153f)},
             {SortingColor.Cyan, Color.cyan},
             {SortingColor.Blank,Color.grey}
         };
